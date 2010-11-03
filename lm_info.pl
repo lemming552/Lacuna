9917f8c0fd49e218ddf839aac80a8ebb06eb2cd0
Paul <logrado@gmail.com>
2010-11-03 18:40:01 -0500

    Only show stats page number and medals earned by default.

--- lm_info.pl
+++ lm_info.pl
@@ -3,6 +3,9 @@ use strict;
 use warnings;
 use Games::Lacuna::Client;
 use YAML::Any;
+use Getopt::Std;
+
+getopts('v', \my %opts) || usage();
 
 my $config_file = shift @ARGV;
 usage() if not defined $config_file or not -e $config_file;
@@ -25,21 +28,28 @@ $SIG{INT} = sub {
 my $stats = $client->stats;
 
 my $info = $stats->find_empire_rank('empire_size_rank', $target_empire);
-print Dump( $info->{empires} );
+print Dump( $info->{empires} ) if $opts{v};
+print "Stats page number: " . $info->{empires}[0]{page_number};
 
 print "\n";
 my $emp_id = $$info{'empires'}[0]{'empire_id'};
 
 my $emp = $client->empire(id => $emp_id);
 my $more_info = $emp->view_public_profile();
-print Dump( $more_info->{profile} );
+print Dump( $more_info->{profile} ) if $opts{v};
+
+my $medals = $more_info->{profile}{medals};
+my $by_name = sub { $medals->{$a}{name} cmp $medals->{$b}{name} };
+foreach my $m_id ( sort $by_name keys %$medals ) {
+    printf "%s (%d times)\n", $medals->{$m_id}{name}, $medals->{$m_id}{times_earned};
+}
 
 exit;
 
 
 sub usage {
   die <<"END_USAGE";
-Usage: $0 myempire.yml  Some Empire Name
+Usage: $0 [-v] myempire.yml  Some Empire Name
 
 END_USAGE
 
