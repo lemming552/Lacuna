f582ead3595fa9cea128f7364be4f5033ecd5f8d
lemming552 <lemming@quirkyqatz.com>
2011-02-18 11:40:37 -0700

    Classified

--- bin/spy_empire.sh
+++
@@ -1,3 +0,0 @@
-:
-cd ~/mine/lacuna/bin
-./spy_attack.pl --config norway.yml --planet Vinland --planet Greenland --planet Copenhagen --task "Gather Empire Intelligence"
--- bin/spy_insurrect.sh
+++
@@ -1,3 +0,0 @@
-:
-cd ~/mine/lacuna/bin
-./spy_attack.pl --config norway.yml --planet Greenland --planet Vinland --planet Copenhagen --task "Incite Insurrection"
