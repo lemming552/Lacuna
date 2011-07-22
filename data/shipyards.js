53f308866c8fc037abd3b6a557074e210ba22bcd
lemming552 <lemming@quirkyqatz.com>
2011-07-21 23:20:37 -0600

    sample shipyard file

---
+++ data/shipyards.js
@@ -0,0 +1,28 @@
+{
+   "PLANETNAME" : {
+      "ShipYard ID" : {
+         "efficiency" : "100",
+         "image" : "shipyard9",
+         "level" : "30",
+         "leveled" : "30",
+         "maxq" : 28,
+         "name" : "Shipyard",
+         "reserve" : 184,
+         "url" : "/shipyard",
+         "x" : "0",
+         "y" : "1"
+      },
+      "ShipYard ID" : {
+         "efficiency" : "100",
+         "image" : "shipyard9",
+         "level" : "30",
+         "leveled" : "30",
+         "maxq" : 28,
+         "name" : "Shipyard",
+         "reserve" : 184,
+         "url" : "/shipyard",
+         "x" : "0",
+         "y" : "2"
+      }
+   }
+}
