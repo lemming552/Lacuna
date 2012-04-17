2580048b03ad4f0ff7d5b22d89c9355619a9aab8
lemming552 <move@quirkyqatz.com>
2012-04-16 23:32:53 -0600

    Better date order.

--- bin/parse_empire.pl
+++ bin/parse_empire.pl
@@ -5,17 +5,20 @@ use warnings;
 use Getopt::Long qw(GetOptions);
 use JSON;
 use Data::Dumper;
+use Time::Piece;
 use utf8;
 binmode STDOUT, ":utf8";
 
 my $data_file = "log/empire_rank.js";
 my $help = 0;
 my $detail = 0;
+my $eburn  = 0;
 
 GetOptions(
   'help' => \$help,
   'input=s' => \$data_file,
   'detail'  => \$detail,
+  'eburn'   => \$eburn,
 );
   if ($help) {
     print "parse_empire.pl --input input\n";
@@ -29,22 +32,75 @@ GetOptions(
   close(DATA);
 
   print "Name\tID\tAlliance\tAID\tDate Created";
-  print "\tColonies\tLast Login" if $detail;
+  print "\tColonies\tLast Login\tAlive" if $detail;
   print "\n";
   for my $empire (sort { $a->{empire_id} <=> $b->{empire_id} } @$file_data) {
+    if ($eburn) {
+      next unless ($empire->{profile}->{last_login});
+      next if (burning($empire->{profile}->{last_login}));
+    }
     my $data = [
       defined($empire->{empire_name}) ? $empire->{empire_name} : "",
       defined($empire->{empire_id}) ? $empire->{empire_id} : "",
       defined($empire->{alliance_name}) ? $empire->{alliance_name} : "",
       defined($empire->{alliance_id}) ? $empire->{alliance_id} : "",
-      defined($empire->{profile}->{date_founded}) ? $empire->{profile}->{date_founded} : ""
+      defined($empire->{profile}->{date_founded}) ? format_date($empire->{profile}->{date_founded}) : ""
     ];
     if ($detail) {
-       push @$data, @{[
+      my $alive = "";
+      if (defined($empire->{profile}->{date_founded}) && 
+          defined($empire->{profile}->{last_login})) {
+         $alive = alive_time( $empire->{profile}->{date_founded},
+                              $empire->{profile}->{last_login});
+      }
+      push @$data, @{[
          defined($empire->{profile}->{colony_count}) ? $empire->{profile}->{colony_count} : "",
-         defined($empire->{profile}->{last_login}) ? $empire->{profile}->{last_login} : "",
+         defined($empire->{profile}->{last_login}) ? format_date($empire->{profile}->{last_login}) : "",
+         $alive,
        ]};
     }
     print join("\t", @$data), "\n";
   }
 exit;
+
+sub format_date {
+  my ($time) = @_;
+
+# 26 11 2011 00:48:50 +0000
+  $time =~ s/ \+0000$//;
+  my $utime = Time::Piece->strptime($time, "%d %m %Y %T");
+  return sprintf("%s", $utime->datetime);
+}
+
+sub alive_time {
+  my ($time1, $time2) = @_;
+  $time1 =~ s/ \+0000$//;
+  $time2 =~ s/ \+0000$//;
+  my $utime1 = Time::Piece->strptime($time1, "%d %m %Y %T");
+  my $utime2 = Time::Piece->strptime($time2, "%d %m %Y %T");
+  return sprintf("%03d:%02d:%02d:%02d", sec2dhms($utime2->epoch - $utime1->epoch));
+}
+
+sub burning {
+  my ($time) = @_;
+
+# 26 11 2011 00:48:50 +0000
+  $time =~ s/ \+0000$//;
+  my $utime = Time::Piece->strptime($time, "%d %m %Y %T");
+  my $gm = gmtime;
+
+  return 1 if ( ($gm->epoch - $utime->epoch) < (21*24*60*60));
+  return 0;
+}
+
+sub sec2dhms
+{
+    use integer;
+    local $_ = shift;
+    my ($d, $h, $m, $s);
+    $s = $_ % 60; $_ /= 60;
+    $m = $_ % 60; $_ /= 60;
+    $h = $_ % 24; $_ /= 24;
+    $d = $_;
+    return ($d, $h, $m, $s);
+}
